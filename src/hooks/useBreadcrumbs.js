7bdab72d50c01c8b71e31602a76079f2c152ad39
Bappy38 <iqbal.bappy.13@gmail.com>
2025-01-21 15:04:41 +0600

    Breadcrumbs fetching from API

--- src/hooks/useBreadcrumbs.js
+++ src/hooks/useBreadcrumbs.js
@@ -1,46 +1,59 @@
 import { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux"
 import { initializeBreadcrumbs } from "../store/breadCrumbSlice";
+import { ENDPOINTS } from "../constants/endpoints";
+import { useParams } from "react-router-dom";
 
 
 const useBreadcrumbs = (currentPath) => {
 
     const breadcrumbs = useSelector((state) => state.breadcrumb);
+    const { folderId } = useParams();
     const dispatch = useDispatch();
 
     useEffect(() => {
+        getBreadcrumbs();
+    }, [ currentPath, dispatch]);
 
-        const fetchBreadcrumbs = () => {
+    const getBreadcrumbs = async() => {
 
-            if (breadcrumbs && breadcrumbs.length > 0) {
-                return;
-            }
+        // if (breadcrumbs && breadcrumbs.length > 0) {
+        //     return;
+        // }
 
-            try {
-                const initialBreadcrumb = {
-                    id: currentPath === '/my-drive'? 'my-drive' : 'shared-drive',
-                    label: currentPath === '/my-drive'? 'My Drive' : 'Shared Drive',
-                    path: currentPath === '/my-drive'? '/my-drive' : '/shared-drive'
-                };
+        const initialBreadcrumb = {
+            id: currentPath.startsWith('/my-drive')? 'my-drive' : 'shared-drive',
+            label: currentPath.startsWith('/my-drive')? 'My Drive' : 'Shared Drive',
+            path: currentPath.startsWith('/my-drive')? '/my-drive' : '/shared-drive'
+        };
 
-                // const response = await axios.get('/api/breadcrumbs', {
-                //     params: {
-                //         folderId: ''
-                //     }
-                // });
+        const fetchedBreadcrumbs = await fetchBreadcrumbs(initialBreadcrumb);
+        const updatedBreadcrumbs = [initialBreadcrumb, ...fetchedBreadcrumbs];
+        dispatch(initializeBreadcrumbs(updatedBreadcrumbs));
+    }
 
-                // const fetchedBreadcrumbs = response.data;
-                const fetchedBreadcrumbs = [];
-                const updatedBreadcrumbs = [initialBreadcrumb, ...fetchedBreadcrumbs];
-                dispatch(initializeBreadcrumbs(updatedBreadcrumbs));
-            }
-            catch (error) {
-                console.error('Error fetching breadcrumbs: ', error);
-            }
+    const fetchBreadcrumbs = async (rootBreadcrumb) => {
+
+        if (!folderId) {
+            return [];
         }
 
-        fetchBreadcrumbs();
-    }, [breadcrumbs, currentPath, dispatch]);
+        try {
+            const endpoint = ENDPOINTS.GET_BREADCRUMBS(folderId);
+            const response = await fetch(endpoint);
+            const json = await response.json();
+
+            const breadcrumbs = json.slice(1).map((folder) => ({
+                id: folder.folderId,
+                path: `${rootBreadcrumb.path}/folders/${folder.folderId}`,
+                label: folder.folderName
+            }));
+            return breadcrumbs;
+        }
+        catch (error) {
+            console.error('Error fetching breadcrumbs: ', error);
+        }
+    }
 
     return breadcrumbs;
 }
