f14126003245bdbc472416304ba3418c3a5abfeb
Bappy38 <iqbal.bappy.13@gmail.com>
2025-02-23 22:33:53 +0600

    Default thumbnail added for image and pdf

--- src/components/FileCard.js
+++ src/components/FileCard.js
@@ -1,10 +1,15 @@
 import { useState } from "react";
 import FilePreview from "./FilePreview";
+import { FILE_TYPES, getFileType } from "../utils/fileTypes";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faFilePdf, faImage } from "@fortawesome/free-solid-svg-icons";
 
 const FileCard = ({file}) => {
 
     const [isPreviewOpen, setIsPreviewOpen] = useState(false);
     const {fileId, fileName, fileType, thumbnailUrl} = file;
+
+    const mappedFileType = getFileType(fileType);
 
     return (
         <>
@@ -22,6 +27,10 @@ const FileCard = ({file}) => {
                                 alt={`${fileName} thumbnail`}
                                 className="rounded-md max-w-full max-h-full object-cover"
                             />
+                        ) : mappedFileType === FILE_TYPES.IMAGE? (
+                            <FontAwesomeIcon className="w-[60%] h-[60%]" icon={faImage} />
+                        ) : mappedFileType === FILE_TYPES.PDF? (
+                            <FontAwesomeIcon className="w-[60%] h-[60%]" icon={faFilePdf} />
                         ) : (
                             <div className="bg-gray-200 text-gray-600 rounded-lg">
                                 <span>Loading...</span>
